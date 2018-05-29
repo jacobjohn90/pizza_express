e83facd159d07f6c4ca2915ae6b2fc824987d0bf
jacobjohn90 <jacobjohn90@gmail.com>
2018-05-29 17:16:34 -0400

    completed GET routes

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Pizza Express')
+})
+app.get('/topping/:type', (req, res) => {
+    res.send(`${req.params.type} pizza! Good choice.`)
+})
+app.get('/order/:amount/:size', (req, res) => {
+    res.send(`Your order of ${req.params.amount} ${req.params.size} pizzas will be ready in 1 minute!`)
+})
+
+
+
+
 app.listen(PORT, ()=> {
     console.log("=========================")
     console.log(`LISTENING ON PORT ${PORT}`)
